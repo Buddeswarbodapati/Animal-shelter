35b7b3f93218534bd1c04d4701d392480d6d6e62
Buddeswarbodapati <buddeswarbodapati@gmail.com>
2025-05-06 19:07:05 +0530

    Add files via upload

---
+++ Client/src/Components/Contact/Contact.js
@@ -0,0 +1,41 @@
+import React from "react";
+import developerPng from "./images/developer-png.png";
+
+const Contact = () => {
+  return (
+    <div className="contactUs-main-container">
+      <div className="contactUs-left-para">
+        <h3>Let's get in touch</h3>
+        <i class="fa fa-envelope"></i>
+        <a class="mail-links" href="mailto:kashifkzmi5@gmail.com">
+          eswar@gmail.com
+        </a>
+
+        <i class="fa fa-linkedin"></i>
+        <a class="mail-links" href="">
+          User Name: ESWAR
+        </a>
+
+        <i class="fa fa-github"></i>
+        <a class="mail-links" href="">
+          ESWAR
+        </a>
+
+        <i class="fa fa-instagram"></i>
+        <a class="mail-links" href="">
+          @eswar_x
+        </a>
+
+        <i class="fa fa-phone"></i>
+        <a class="mail-links" href="tel:+9876543210">
+          +9876543210
+        </a>
+      </div>
+      <div className="contactUs-pic">
+        <img src={developerPng} alt="Profile"/>
+      </div>
+    </div>
+  );
+};
+
+export default Contact; 
